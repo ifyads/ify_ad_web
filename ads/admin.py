d662f63cef9a693e2087256934dae96961a7d0c6
ifyads <chidambaram5@zohomail.in>
2024-06-29 19:18:47 +0530

    Delete ads directory

--- ads/admin.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template ,request
-
-
-app = Flask(__name__)
-
-def admin():
-   if request.method == "GET":
-       return render_template("/admin/login.html")
-
--- ads/b3cd9LT.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template 
-
-
-app = Flask(__name__)
-
-def ClRa1():
-       return render_template("/admin/log625/A6dM2iN5.html")
-       
